b40ab6e8fa885d6af550bc5e1a23df5488073877
BowDown097 <btd6modsyeet@gmail.com>
2024-07-29 22:25:49 -0700

    Change leaderboard max page to -1

--- src/paginators/leaderboardpaginator.h
+++ src/paginators/leaderboardpaginator.h
@@ -8,7 +8,7 @@ public:
     explicit LeaderboardPaginator(std::string_view currency, long double currencyValue, int64_t guildId)
         : currency(currency), currencyValue(currencyValue), guildId(guildId) {}
     dpp::interaction_page get_or_load_page(int pageIndex) override;
-    int max_page_index() const override { return 0; }
+    int max_page_index() const override { return -1; }
 private:
     std::string currency;
     long double currencyValue;
